68d5c42b9928383842d7d9d9c584354abeddbd2b
Banka24 <Aq567489012@gmail.com>
2023-12-17 12:33:53 +0300

    Добавьте файлы проекта.

---
+++ NewYearGame/NewYearGame/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace NewYearGame
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void StartGameButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
