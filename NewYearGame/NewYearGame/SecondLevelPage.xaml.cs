4bda97086cff19a53f7cd8210d59134f3066cfd3
Banka24 <Aq567489012@gmail.com>
2023-12-17 18:51:12 +0300

    add SecondLevel

--- NewYearGame/NewYearGame/SecondLevelPage.xaml.cs
+++ NewYearGame/NewYearGame/SecondLevelPage.xaml.cs
@@ -3,33 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Threading;
+
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace NewYearGame
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class FirstLevelPage : ContentPage
+    public partial class SecondLevelPage : ContentPage
     {
-        public FirstLevelPage()
+        public SecondLevelPage()
         {
-            DisplayAlert("История", "На носу Новый Год, а злые снеговики украли нашу ёлку.\nСовсем распоясались.\nНадо успеть вернуть зелёную, пока моя внучка не вернулась.", "Начнём");
             Random random = new Random();
             InitializeComponent();
-            Grid.SetColumn(Enemy, random.Next(1, 4));
-            Grid.SetRow(Enemy, random.Next(1, 4));
-            Grid.SetColumn(Elka, random.Next(1, 4));
-            Grid.SetRow(Elka, random.Next(1, 4));
-        }        
-
+            Grid.SetColumn(Enemy, random.Next(1, 7));
+            Grid.SetRow(Enemy, random.Next(1, 7));
+            Grid.SetColumn(Elka, random.Next(1, 7));
+            Grid.SetRow(Elka, random.Next(1, 7));
+        }
 
         private void CheckStatus(Image elka, in int colPlayer, in int rowPlayer)
         {
             int colElka = Grid.GetColumn(elka);
             int rowElka = Grid.GetRow(elka);
 
-            if(colElka == colPlayer && rowElka == rowPlayer)
+            if (colElka == colPlayer && rowElka == rowPlayer)
             {
                 DisplayAlert("Победа", "Ваша ёлка в другом лесу", "Продолжить");
                 Navigation.PushAsync(new SecondLevelPage());
@@ -39,11 +37,11 @@ namespace NewYearGame
         private void MoveEnemy(Image enemy, in int colPlayer, in int rowPlayer)
         {
             Random random = new Random();
-            int col = random.Next(4);
-            int row = random.Next(4);
+            int col = random.Next(7);
+            int row = random.Next(7);
             Grid.SetColumn(enemy, col);
             Grid.SetRow(enemy, row);
-            if(col == colPlayer && row == rowPlayer)
+            if (col == colPlayer && row == rowPlayer)
             {
                 DisplayAlert("Проигрыш", "Не расстраивайтесь, такое бывает.\nПопробуйте заново", "Ok");
                 Navigation.PopToRootAsync();
@@ -62,18 +60,18 @@ namespace NewYearGame
                     break;
                 case SwipeDirection.Left:
                     col--;
-                    break; 
+                    break;
                 case SwipeDirection.Right:
                     col++;
-                    break; 
+                    break;
                 case SwipeDirection.Down:
                     row++;
                     break;
             }
 
-            col = Math.Max(0, Math.Min(col, 4));
-            row = Math.Max(0, Math.Min(row, 4));
-                        
+            col = Math.Max(0, Math.Min(col, 7));
+            row = Math.Max(0, Math.Min(row, 7));
+
             MoveEnemy(Enemy, col, row);
             CheckStatus(Elka, col, row);
 
