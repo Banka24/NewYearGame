2b8b233ca4180afc5c7b3a2b42b365836920ca81
Banka24 <Aq567489012@gmail.com>
2023-12-17 19:05:18 +0300

    make version 0.1.0

---
+++ NewYearGame/NewYearGame/ThirdLevelPage.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NewYearGame
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ThirdLevelPage : ContentPage
+    {
+        public ThirdLevelPage()
+        {
+            Random random = new Random();
+            InitializeComponent();
+            Grid.SetColumn(Enemy, random.Next(1, 7));
+            Grid.SetRow(Enemy, random.Next(1, 7));
+            Grid.SetColumn(Elka, random.Next(1, 7));
+            Grid.SetRow(Elka, random.Next(1, 7));
+        }
+        private void CheckStatus(Image elka, in int colPlayer, in int rowPlayer)
+        {
+            int colElka = Grid.GetColumn(elka);
+            int rowElka = Grid.GetRow(elka);
+
+            if (colElka == colPlayer && rowElka == rowPlayer)
+            {
+                DisplayAlert("Победа", "Это ваша ёлка. Новы год спасён", "Продолжить");
+                Navigation.PopToRootAsync();
+            }
+        }
+
+        private void MoveEnemy(Image enemy, in int colPlayer, in int rowPlayer)
+        {
+            Random random = new Random();
+            int col = random.Next(7);
+            int row = random.Next(7);
+            Grid.SetColumn(enemy, col);
+            Grid.SetRow(enemy, row);
+            if (col == colPlayer && row == rowPlayer)
+            {
+                DisplayAlert("Проигрыш", "Не расстраивайтесь, такое бывает.\nПопробуйте заново", "Ok");
+                Navigation.PopToRootAsync();
+            }
+        }
+
+        private void SwipeGestureRecognizer_Swiped(object sender, SwipedEventArgs e)
+        {
+            int col = Grid.GetColumn(this.Player);
+            int row = Grid.GetRow(this.Player);
+
+            switch (e.Direction)
+            {
+                case SwipeDirection.Up:
+                    row--;
+                    break;
+                case SwipeDirection.Left:
+                    col--;
+                    break;
+                case SwipeDirection.Right:
+                    col++;
+                    break;
+                case SwipeDirection.Down:
+                    row++;
+                    break;
+            }
+
+            col = Math.Max(0, Math.Min(col, 7));
+            row = Math.Max(0, Math.Min(row, 7));
+
+            MoveEnemy(Enemy, col, row);
+            CheckStatus(Elka, col, row);
+
+            Grid.SetColumn(this.Player, col);
+            Grid.SetRow(this.Player, row);
+        }
+    }
+}
