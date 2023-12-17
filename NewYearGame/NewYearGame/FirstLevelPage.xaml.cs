9e8a68a17e1109d7622258febe19a605f90e4706
Banka24 <Aq567489012@gmail.com>
2023-12-17 17:39:26 +0300

    add AI for Enemy

--- NewYearGame/NewYearGame/FirstLevelPage.xaml.cs
+++ NewYearGame/NewYearGame/FirstLevelPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Threading;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -14,7 +14,55 @@ namespace NewYearGame
     {
         public FirstLevelPage()
         {
+            Random random = new Random();
             InitializeComponent();
+            Grid.SetColumn(Enemy, random.Next(4));
+            Grid.SetRow(Enemy, random.Next(4));
+            
+        }        
+
+        private void MoveEnemy(Image enemy, in int colPlayer, in int rowPlayer)
+        {
+            Random random = new Random();
+            int col = random.Next(4);
+            int row = random.Next(4);
+            Grid.SetColumn(enemy, col);
+            Grid.SetRow(enemy, row);
+            if(col == colPlayer && row == rowPlayer)
+            {
+                DisplayAlert("Проигрыш", "Не расстраивайтесь, такое бывает.\nПопробуйте заново", "Restart");
+                Navigation.PushAsync(new FirstLevelPage());
+            }
+        }
+
+        private void SwipeGestureRecognizer_Swiped(object sender, SwipedEventArgs e)
+        {
+            int col = Grid.GetColumn(this.Player);
+            int row = Grid.GetRow(this.Player);
+
+            switch (e.Direction)
+            {
+                case SwipeDirection.Up:
+                    row--;
+                    break;
+                case SwipeDirection.Left:
+                    col--;
+                    break; 
+                case SwipeDirection.Right:
+                    col++;
+                    break; 
+                case SwipeDirection.Down:
+                    row++;
+                    break;
+            }
+
+            col = Math.Max(0, Math.Min(col, 4));
+            row = Math.Max(0, Math.Min(row, 4));
+
+            MoveEnemy(Enemy, col, row);
+
+            Grid.SetColumn(this.Player, col);
+            Grid.SetRow(this.Player, row);
         }
     }
 }
